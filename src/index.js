95d88bc4be4e5a3e88de18c913324765a7464ae0
saranyaveeradurai <saranya@dcsipl.com>
2022-04-12 15:03:43 +0530

    second page created with tab
    second page created with tab

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import Header from './header.js';
-import FirstHalf from './firsthalf.js';
-import SecondHalf from './secondhalf.js';
+// import FirstHalf from './firsthalf.js';
+// import SecondHalf from './secondhalf.js';
 import SecondPage from './secondpage.js';
 import Footer from './footer.js';
 import './header.css';
-import './firsthalf.css';
-import './secondhalf.css';
-import './footer.css'
+// import './firsthalf.css';
+// import './secondhalf.css';
+import './secondpage.css';
+import './footer.css';
 
 export default function Main(){
     return(
     <>
        <Header/>
-       <FirstHalf/>
-       <SecondHalf/>
+       {/* <FirstHalf/>
+       <SecondHalf/> */}
+       <SecondPage/>
        <Footer/>
     </>
     )
